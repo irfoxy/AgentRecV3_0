5bb83c247b7033ac3943de2c845209a4e6acaecb
icfoxy <2786396952@qq.com>
2025-09-26 15:20:30 +0800

    0926A

--- model/RecAgent.py
+++ model/RecAgent.py
@@ -1,12 +1,85 @@
-from LLM import GPT
 from langgraph.checkpoint.memory import MemorySaver
 from typing import List
+from langgraph.prebuilt import create_react_agent
+import json
+import re
+
+SYSTEM_PROMPT="""
+You are an expert in {} recommendation.
+You will receive the user's browsing history, including item IDs, item descriptions, and user ratings.
+Another recommendation system will provide several recommendation candidates for reference.
+After that, you will select one item from a candidate list as the final recommendation.
+Your response must follow the JSON format below:
+{"rec_id": INT, "explain": STRING}
+Note: You must return PURE JSON only.
+Think carefully before answering.
+"""
+
+FORWARD_PROMPT="""
+User history:
+{}
+Candidate list:
+{}
+Another recommendation system provided the following recommendation list:
+{}
+"""
+
+RETRY_PROMPT="""
+The user does not accept your recommendation.
+The reasons are as follows:
+{}
+Based on the above information, analyze again and make a new recommendation.
+"""
+
+def extract_json(text: str) -> dict:
+    match = re.search(r'\{.*\}', text, re.DOTALL)
+    if not match:
+        raise ValueError("JSON NOT FOUND")
+    
+    json_str = match.group(0).strip()
+    
+    try:
+        return json.loads(json_str)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"JSON ERROR: {e}")
+    
 
 class RecAgent:
-    def __init__(self,thread_id:str,tools:List):
-        self.llm=GPT
+    def __init__(self,model,thread_id:str,tools:List,item_type:str):
         self.memory=MemorySaver()
-        self.config={"condigurable":{"thread_id":thread_id}}
+        self.config = {"configurable": {"thread_id": thread_id}}
 
-    def forward(self,history:str,candidate:str)->int:
-        pass
+        self.model=create_react_agent(model=model,tools=tools,checkpointer=self.memory)
+        
+        self.msgs=[{"role":"system","content":SYSTEM_PROMPT.format(item_type)}] 
+    
+    def run(self,is_print=False):
+        resp=self.model.invoke({"messages":self.msgs},config=self.config)
+
+        if is_print:
+            for msg in resp['messages']:
+                msg.pretty_print()
+
+        try:
+            content=resp['messages'][-1].content
+            ans=extract_json(text=content)
+        except Exception as e:
+            raise e
+        
+        return ans
+
+    def forward(self,history_lst:str,candidate_lst:str,predicted:str)->int:
+        self.msgs.append({"role":"user","content":FORWARD_PROMPT.format(history_lst,candidate_lst,predicted)})
+        ans=self.run()
+
+        return ans
+
+    def retry(self,feed_back:str):
+        self.msgs.append({"role":"user","content":RETRY_PROMPT.format(feed_back)})
+        ans=self.run()
+
+        return ans
+
+
+
+    
---
+++ test.py
@@ -0,0 +1,16 @@
+
+# from model.LLM import GPT
+# from model.RecAgent import RecAgent
+# from dotenv import load_dotenv
+
+# # load_dotenv(dotenv_path='./.env')
+
+
+# if __name__=="__main__":
+#     rec_agent=RecAgent(model=GPT,thread_id="test@1",tools=[],item_type="movie")
+
+
+#     resp=rec_agent.run(is_print=True)
+
+#     print("----------------------------------------------")
+#     print(resp)
